28d4b96646e84d923688262aacad2720b03fe264
BlackstarMC97 <penayecyrille@gmail.com>
2018-04-30 16:31:43 +0100

    Changement du favicon

--- header.php
+++ header.php
@@ -2,21 +2,25 @@
     <div class="top-bar">
         <div class="container">
             <div class="row">
-                <div class="col-sm-6 col-xs-4">
-                    <div class="top-number"><p><i class="fa fa-phone-square"></i>  +0123 456 70 90</p></div>
+                <div class="col-sm-8 col-xs-4">
+                    <div class="top-number">
+                        <p>
+                            <i class="fa fa-phone-square"></i>  +237 655 669 952 
+                            <i class="fa fa-envelope" style="margin-left:5%;"></i>  contact@mboageek.com
+                            <i class="fa fa-map-marker" style="margin-left:5%;"></i>  Makepe Missoke, Douala Cameroun
+                        </p>
+                    </div>
                 </div>
-                <div class="col-sm-6 col-xs-8">
+                <div class="col-sm-4 col-xs-8">
                    <div class="social">
                         <ul class="social-share">
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                             <li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fa fa-skype"></i></a></li>
                         </ul>
                         <div class="search">
                             <form role="form">
-                                <input type="text" class="search-form" autocomplete="off" placeholder="Search">
+                                <input type="text" class="search-form" autocomplete="off" placeholder="<?php echo L::recherche.'...'; ?>">
                                 <i class="fa fa-search"></i>
                             </form>
                        </div>
--- langcache/php_i18n_f29c0806a448cd8ac2e2139df3e063ad_L_fr.cache.php
+++ langcache/php_i18n_f29c0806a448cd8ac2e2139df3e063ad_L_fr.cache.php
@@ -1,5 +1,6 @@
 <?php class L {
 const accueil = 'Accueil';
+const recherche = 'Rechercher';
 const title_accueil = 'Accueil | MboaGeek';
 public static function __callStatic($string, $args) {
     return vsprintf(constant("self::" . $string), $args);
