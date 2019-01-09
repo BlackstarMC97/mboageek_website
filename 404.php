de5e864c66daea7ea98757ffe81321515d67d782
BlackstarMC97 <penayecyrille@gmail.com>
2019-01-09 16:21:58 +0100

    Error 404 page update

--- 404.php
+++ 404.php
@@ -14,6 +14,7 @@
         <h1><?php echo L::error_msg; ?></h1>
         <p><?php echo L::error_texte; ?></p>
         <a class="btn btn-primary" href="index.php"><?php echo L::error_btntext; ?></a>
+        <img class="img-responsive" style="margin:0 auto; max-height: 300px;" src="images/page-not-found.png">
     </section><!--/#error-->
 
     <?php include("footer.php"); ?>
